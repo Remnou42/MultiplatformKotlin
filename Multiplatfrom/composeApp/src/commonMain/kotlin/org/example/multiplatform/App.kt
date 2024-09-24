d83fdd954c4984be6878cacd6a8cbdea621ad6d0
Rémi NOUVEAU <r.nouveau@pickup-services.com>
2024-09-24 11:04:01 +0200

    feat: app with gpioset

---
+++ Multiplatfrom/composeApp/src/commonMain/kotlin/org/example/multiplatform/App.kt
@@ -0,0 +1,44 @@
+package org.example.multiplatform
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import org.jetbrains.compose.resources.painterResource
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+import multiplatfrom.composeapp.generated.resources.Res
+import multiplatfrom.composeapp.generated.resources.compose_multiplatform
+import java.io.DataOutputStream
+
+@Composable
+@Preview
+fun App() {
+    val process = Runtime.getRuntime().exec("bash")
+    val os = DataOutputStream(process.outputStream)
+
+    os.writeBytes("gpioset gpiochip0 17=1")
+    os.flush()
+
+    MaterialTheme {
+        var showContent by remember { mutableStateOf(false) }
+        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+            Button(onClick = { showContent = !showContent }) {
+                Text("Click me!")
+            }
+            AnimatedVisibility(showContent) {
+                val greeting = remember { Greeting().greet() }
+                Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+                    Image(painterResource(Res.drawable.compose_multiplatform), null)
+                    Text("Compose: $greeting")
+                }
+            }
+        }
+    }
+}
